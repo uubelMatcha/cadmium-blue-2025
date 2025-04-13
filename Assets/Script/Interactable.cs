32fe18ee0cdf375f4207e3e73530a5f89f81ff0c
stevenleej <koreancoffe@gmail.com>
2025-04-13 16:28:26 -0400

    add quick sorting hack

--- Assets/Script/Interactable.cs
+++ Assets/Script/Interactable.cs
@@ -1,46 +1,66 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Interactable : MonoBehaviour
+namespace Script
 {
+    public class Interactable : MonoBehaviour
+    {
 
 
-    public GameObject text;
-    public InteractableData data;
+        public GameObject text;
+        public InteractableData data;
 
-    private void OnTriggerEnter2D(Collider2D other) {
+        private void OnTriggerEnter2D(Collider2D other) {
 
-        if (other.tag == "PlayerInteractionHitBox") {
-            text.SetActive(true);
+            if (other.tag == "PlayerInteractionHitBox") {
+                text.SetActive(true);
 
-            PlayerInteraction playerInteraction = other.GetComponent<PlayerInteraction>();
+                PlayerInteraction playerInteraction = other.GetComponent<PlayerInteraction>();
 
-            AudioManager.audioManagerInstance.PlaySoundEffect(data.sfx.musicTitle);
+                AudioManager.audioManagerInstance.PlaySoundEffect(data.sfx.musicTitle);
             
-            float anxiety = FindFirstObjectByType<AnxietySystem>().anxietyLevel;
+                float anxiety = FindFirstObjectByType<AnxietySystem>().anxietyLevel;
 
-            if (anxiety <= 0.33f) {
-                playerInteraction.SetTextData(data.level1);
+                if (anxiety <= 0.33f) {
+                    playerInteraction.SetTextData(data.level1);
+                }
+                else if (anxiety <= 0.66f) {
+                    playerInteraction.SetTextData(data.level2);
+                }
+                else {
+                    playerInteraction.SetTextData(data.level3);
+                }
             }
-            else if (anxiety <= 0.66f) {
-                playerInteraction.SetTextData(data.level2);
-            }
-            else {
-                playerInteraction.SetTextData(data.level3);
-            }
-        }
-
-    }
-
-    private void OnTriggerExit2D(Collider2D other) {
-
-
-        if (other.tag == "PlayerInteractionHitBox") {
-            text.SetActive(false);
-            PlayerInteraction playerInteraction = other.GetComponent<PlayerInteraction>();
-
-            playerInteraction.ResetData();
 
         }
 
+        private void OnTriggerExit2D(Collider2D other) {
+
+
+            if (other.tag == "PlayerInteractionHitBox") {
+                text.SetActive(false);
+                PlayerInteraction playerInteraction = other.GetComponent<PlayerInteraction>();
+
+                playerInteraction.ResetData();
+
+            }
+
+        }
+
+        private void Update()
+        {
+            GameObject other = GameObject.FindGameObjectWithTag("Player");
+            //hack
+            if ((other.transform.position.y > transform.position.y) && (other.GetComponent<SpriteRenderer>() != null))
+            {
+                //higher than the ghost boy
+                gameObject.GetComponentInChildren<SpriteRenderer>().sortingOrder = other.GetComponent<SpriteRenderer>().sortingOrder + 1;
+            }
+            else if(other.transform.position.y <= transform.position.y)
+            {
+                gameObject.GetComponentInChildren<SpriteRenderer>().sortingOrder = other.GetComponent<SpriteRenderer>().sortingOrder - 1;
+            }
+        }
     }
 }

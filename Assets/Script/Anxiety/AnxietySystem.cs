4b4fda77c1e5d428ff23a2ee5904d5c4ee4bf2ad
dariusvalevicius <darius.valevicius@gmail.com>
2025-04-12 21:07:40 -0400

    Ignore button scaling

--- Assets/Script/Anxiety/AnxietySystem.cs
+++ Assets/Script/Anxiety/AnxietySystem.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,6 +15,11 @@ public class AnxietySystem : MonoBehaviour
     [Header("UI Elements")]
     [SerializeField] private Slider anxietySlider;
     [SerializeField] private Button ignoreButton;
+    // [SerializeField] private Image ignoreButtonCompletionSlider;
+
+
+    public float maxIgnoreButtonScale = 1.5f;
+    public float ignoreButtonRelaxTime = 0.25f;
     
     private bool isTickingAnxiety = false;
     private int curIgnoreClicks = 0;
@@ -29,6 +37,7 @@ public class AnxietySystem : MonoBehaviour
         MessageSystem.Instance.onMessageCloseDelayPassed += OnMessageCloseDelayPassed;
 
         postProcessingBehaviour = FindFirstObjectByType<PostProcessingBehaviour>();
+        // ignoreButtonCompletionSlider.gameObject.transform.localScale = new Vector3(0f, 1f, 1f);
     }
     
     private void OnMessageOpen(bool isBadMessage)
@@ -47,12 +56,17 @@ public class AnxietySystem : MonoBehaviour
         isTickingAnxiety = false;
         ignoreButton.gameObject.SetActive(false);
         postProcessingBehaviour.panicMode = false;
+
+        ignoreButton.transform.localScale = new Vector3(1f, 1f, 1f);
+        // ignoreButtonCompletionSlider.gameObject.transform.localScale = new Vector3(0f, 1f, 1f);
+
+
     }
 
     // TODO: make it time frame independent?
     private void Update()
     {
-
+        // Update exposed anxiety level from 0 to 1 for dependent objects
         anxietyLevel = anxietySlider.value / 100f;
 
         // checks if the player has clicked ignore enough times
@@ -64,15 +78,18 @@ public class AnxietySystem : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                if (hasMissedDelay)
-                {
-                    MessageSystem.ClosePopUp();
-                }
-                else
-                {
-                    curIgnoreClicks++;
-                    TickAnxietyDown();
-                }
+                // if (hasMissedDelay)
+                // {
+                //     MessageSystem.ClosePopUp();
+                // }
+                // else
+                // {
+                curIgnoreClicks++;
+                // ignoreButtonCompletionSlider.gameObject.transform.localScale = new Vector3(curIgnoreClicks / clicksUntilCloseMessage, 1f, 1f);
+                StopCoroutine("PulseIgnoreButton");
+                StartCoroutine("PulseIgnoreButton");
+                // TickAnxietyDown();
+                // }
             }
         }
     }
@@ -106,5 +123,27 @@ public class AnxietySystem : MonoBehaviour
 
             MessageSystem.ClosePopUp();
         }  
+    }
+
+    private IEnumerator PulseIgnoreButton() {
+
+        float timer = 0f;
+
+        while (timer <= ignoreButtonRelaxTime) {
+
+            float scale = Mathf.Lerp(maxIgnoreButtonScale, 1f, timer / ignoreButtonRelaxTime);
+            ignoreButton.transform.localScale = new Vector3(scale, scale, 1f);
+
+            timer += Time.deltaTime;
+            
+            // if (Input.GetKeyDown(KeyCode.Space))
+            // {
+                // yield break;
+            // }
+            // else {
+            yield return null;
+            // }
+        }
+
     }
 }
